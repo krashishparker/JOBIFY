528ff4c5af0088715b3b03642f65f2786cdbb849
krashishparker <ak970407@gmail.com>
2021-04-22 15:25:09 +0530

    initial commit

---
+++ api/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APP_ID: '98914ca5',
+  API_KEY: 'd7320de30334b3d04b77d001783c7e07',
+  BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+  BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
+};
